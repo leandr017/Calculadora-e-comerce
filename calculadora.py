a9ed0d13cb47bc7eb9f10d8fbd86360a9cb0d5dd
Leandr017 <LeandroContreras017@gmail.com>
2026-09-12 16:55:33 -0300

    Agrego rama calculadora y variables

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+print("CALCULADORA ECOMMERCE")
+print("v1")
+print("Calculos de beneficio")
+
+preciov = input("Precio de venta: ")
+costop = input("Costo por producto: ")
+comisionp = input("Comision por producto: ")
+enviop = input("Envio por producto: ")
+publicidadt = input("Costo publicidad total: ")
+cantv = input("Cantidad de ventas: ")
+
+
+
+
+
+
+
+
+
+
+
