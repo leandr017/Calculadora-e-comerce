0988e16b388032de8d78650db0c5eb11e15515c3
Leandr017 <LeandroContreras017@gmail.com>
2026-09-12 17:14:01 -0300

    Agrego calculos basicos

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,29 @@
 print("CALCULADORA ECOMMERCE")
 print("v1")
-print("Calculos de beneficio")
+print("Calculos de basicos")
 
-preciov = input("Precio de venta: ")
-costop = input("Costo por producto: ")
-comisionp = input("Comision por producto: ")
-enviop = input("Envio por producto: ")
-publicidadt = input("Costo publicidad total: ")
-cantv = input("Cantidad de ventas: ")
+precioV = input("Precio de venta: ")
+costoP = input("Costo por producto: ")
+comisionP = input("Comision por producto: ")
+envioP = input("Envio por producto: ")
+publicidadT = input("Costo publicidad total: ")
+cantV = input("Cantidad de ventas: ")
+
+ingresosT = int(precioV * cantV)
+costoP = int(costoP * cantV)
+comisionT = int(comisionP * cantV)
+envioT = int(envioP * cantV)
+publicidadP = float(publicidadT / cantV)
+gananciaT = int(ingresosT - costoT)
+costoT = int(costoP - comisionP - envioP - publicidadP)
+gananciaP = float(gananciaT / cantV)
+margen = int((gananciaT - costoT)/gananciaT *100)
+roas = float(ingresosT/publicidadT)
+
+
+
+
+print("Ingresos totales:")
 
 
 
