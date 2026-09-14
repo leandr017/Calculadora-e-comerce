c4fb68f7dc0bfb970275019a7d4f396d6f54bc9c
Leandr017 <LeandroContreras017@gmail.com>
2026-09-14 15:39:08 -0300

    Agrego prints resultados

--- calculadora.py
+++ calculadora.py
@@ -2,33 +2,39 @@ print("CALCULADORA ECOMMERCE")
 print("v1")
 print("Calculos de basicos")
 
-precioV = input("Precio de venta: ")
-costoP = input("Costo por producto: ")
-comisionP = input("Comision por producto: ")
-envioP = input("Envio por producto: ")
-publicidadT = input("Costo publicidad total: ")
-cantV = input("Cantidad de ventas: ")
+precioV = int(input("Precio de venta: "))
+costoP = int(input("Costo por producto: "))
+comisionP = int(input("Comision por producto: "))
+envioP = int(input("Envio por producto: "))
+publicidadT = int(input("Costo publicidad total: "))
+cantV = int(input("Cantidad de ventas: "))
 
+publicidadP = float(publicidadT / cantV)
+costoTP = int(costoP * cantV)
 ingresosT = int(precioV * cantV)
-costoP = int(costoP * cantV)
 comisionT = int(comisionP * cantV)
 envioT = int(envioP * cantV)
-publicidadP = float(publicidadT / cantV)
+costoT = int(costoTP + comisionT + envioT + publicidadT)
 gananciaT = int(ingresosT - costoT)
-costoT = int(costoP - comisionP - envioP - publicidadP)
 gananciaP = float(gananciaT / cantV)
-margen = int((gananciaT - costoT)/gananciaT *100)
+margen = int((gananciaT)/ingresosT *100)
 roas = float(ingresosT/publicidadT)
+i = gananciaT + publicidadT
+i = i / ingresosT
+float(breakeven = 1/i)
 
 
-
-
-print("Ingresos totales:")
-
-
-
-
-
+print("RESULTADOS")
+print(f"Ingresos totales:{ingresosT}")
+print(f"Costos Totales:{costoT}")
+print(f"Comision Total:{comisionT}")
+print(f"Envio Total:{envioT}")
+print(f"Costo de publicidad por producto:{publicidadP}")
+print(f"Ganancia total:{gananciaT}")
+print(f"Ganancia por producto:{gananciaP}")
+print(f"Margen:{margen}%")
+print(f"Roas:{roas}")
+print(f"Roas Break-even:{breakeven}")
 
 
 
